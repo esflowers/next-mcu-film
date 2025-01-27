8f716cdecc37aa0d08739adedf5e7901d6d35f2a
esflowers <estrada.flsw@gmail.com>
2025-01-26 23:08:20 -0600

    add proxy, script and create canvas, improve structure

--- index.php
+++ index.php
@@ -16,15 +16,19 @@
 
     <main>
         <section class="col">
-            <img class="poster" src="<?= $movie["poster_url"]; ?>" alt="<?= "The next MCU film is " . $movie["title"]; ?>" loading="lazy">
+            <img class="poster" src="proxy.php?url=<?= urlencode($movie["poster_url"]) ?>" alt="<?= "The next MCU film is " . $movie["title"]; ?>" crossorigin="anonymous">
         </section>
 
         <hgroup class="col">
             <h1><?= $movie["title"]; ?></h1>
-            <h4><?= $next_movie->get_until_messages(); ?></h4>
+            <h5><?= $next_movie->get_until_messages(); ?></h5>
             <p>Fecha de estreno: <?= $movie["release_date"]; ?></p>
             <p><small>El siguiente estreno es <?= $movie["following_production"]; ?></small></p>
         </hgroup>
-    </main> 
+    </main>
+
+    <canvas id="canvas"></canvas>
+
+    <script src="./script.js"></script>
 </body>
 </html>
