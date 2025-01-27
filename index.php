20bb0b74e9b449f2f228d77d3ab25643d2103838
esflowers <estrada.flsw@gmail.com>
2025-01-26 21:50:51 -0600

    fix basic information

--- index.php
+++ index.php
@@ -3,25 +3,27 @@
     require_once './classes/NextMovie.php';
 
     $next_movie = NextMovie::fetch_and_create_movie(API_URL);
-    $next_movie_data = $next_movie->get_data();
+    $movie = $next_movie->get_data();
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
-<?php render_template('head', ["title" => $next_movie_data["title"]]); ?>
+<?php render_template('head', ["title" => $movie["title"]]); ?>
 
 <body>
+    <div class="gradient"></div>
+
     <main>
-        <section>
-            <img src="<?= $next_movie_data["poster_url"]; ?>" alt="<?= $next_movie_data["title"]; ?>" width="250">
+        <section class="col">
+            <img class="poster" src="<?= $movie["poster_url"]; ?>" alt="<?= "The next MCU film is " . $movie["title"]; ?>" loading="lazy">
         </section>
 
-        <hgroup>
-            <h2><?= $next_movie_data["title"]; ?></h2>
+        <hgroup class="col">
+            <h1><?= $movie["title"]; ?></h1>
             <h4><?= $next_movie->get_until_messages(); ?></h4>
-            <p>Fecha de estreno: <?= $next_movie_data["release_date"]; ?></p>
-            <p><small>El siguiente estreno es <?= $next_movie_data["following_production"]; ?></small></p>
+            <p>Fecha de estreno: <?= $movie["release_date"]; ?></p>
+            <p><small>El siguiente estreno es <?= $movie["following_production"]; ?></small></p>
         </hgroup>
     </main> 
 </body>
