732dea7ccace31560b0b216b683c789fdf5bc57e
esflowers <estrada.flsw@gmail.com>
2025-01-26 23:07:46 -0600

    create and add favicon

--- templates/head.php
+++ templates/head.php
@@ -2,5 +2,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title; ?> - The next MCU film</title>
+    <link rel="shortcut icon" href="./favicon.svg" type="image/x-icon">
     <link rel="stylesheet" href="./style.css">
 </head>
