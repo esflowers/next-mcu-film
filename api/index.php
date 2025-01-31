21b7fc9a5947d5442b8713c575f28f46b1f5850f
esflowers <estrada.flsw@gmail.com>
2025-01-30 22:52:13 -0600

    create content for previous and next premiere

--- api/index.php
+++ api/index.php
@@ -2,14 +2,20 @@
     require_once 'functions.php';
     require_once 'classes/NextMovie.php';
 
-    $next_movie = NextMovie::fetch_and_create_movie(API_URL);
     $steps = isset($_GET["next"]) ? (int) $_GET["next"] : 0;
+    $date = isset($_GET["date"]) ? $_GET["date"] : null;
     
-    if ($steps > 0) {
-        $movie = $next_movie->get_following_production($steps);
+    if ($steps > 0 && $date) {
+        $next_movie = NextMovie::fetch_and_create_movie(API_URL, $date);
     } else {
-        $movie = $next_movie->get_data();
+        $next_movie = NextMovie::fetch_and_create_movie(API_URL);
     }
+
+    if (!$next_movie) {
+        die("<h1>No more upcoming MCU movies or shows.</h1>");
+    }
+
+    $movie = $next_movie->get_data();
 ?>
 
 <!DOCTYPE html>
@@ -22,12 +28,12 @@
 
     <main>
         <section class="col">
-            <img class="poster" src="<?= $movie["poster_url"] ?>" alt="<?= "The next MCU film is " . $movie["title"]; ?>" crossorigin="anonymous">
+            <img class="poster" src="<?= $movie["poster_url"] ?>" alt="<?= "The next MCU film is " . $movie["title"]; ?>" crossorigin="anonymous" width="350" height="380" loading="lazy">
         </section>
 
         <hgroup class="col">
             <h1><?= $movie["title"]; ?></h1>
-            <h5><?= $next_movie->get_until_messages(); ?></h5>
+            <h2><?= $next_movie->get_until_messages(); ?></h2>
             <div class="flex gap-md my">
                 <div class="flex" title="Premiere">
                     <?php render_template('iconCalendar') ?>
@@ -39,15 +45,25 @@
                 </div>
             </div>
             <p class="description my"><?= $movie["overview"]; ?></p>
-            <a href="?next=<?= $steps + 1 ?>" class="flex my link">
-                Next premiere 
-                <?php render_template('iconArrow') ?>
-            </a>
+            <div class="flex gap-md">
+                <?php if ($steps > 0): ?>
+                    <button id="previous" class="flex my link prev">
+                        <?php render_template('iconArrow') ?>
+                        Previous premiere
+                    </button>
+                <?php endif; ?>
+                <?php if (!empty($movie["following_production"])): ?>
+                    <a href="?next=<?= $steps + 1 ?>&date=<?= $movie["release_date"] ?>" class="flex my link next">
+                        Next premiere
+                        <?php render_template('iconArrow') ?>
+                    </a>
+                <?php endif; ?>
+            </div>
         </hgroup>
     </main>
 
     <canvas id="canvas"></canvas>
 
-    <script src="./script.js"></script>
+    <script src="./script.js" defer></script>
 </body>
 </html>
